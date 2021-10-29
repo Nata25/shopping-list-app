65c440160795782ccd7a3e600a994ba649ad1074
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-29 22:45:28 +0300

    Basic component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeComponent } from './recipes/recipe/recipe.component';
+import { RecipesComponent } from './recipes/recipes.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipesListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    RecipeComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
