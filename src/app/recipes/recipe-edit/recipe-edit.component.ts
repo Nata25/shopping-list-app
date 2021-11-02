ec5b85298c3f955cff35cbc549fe7d1988af7060
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-11-02 16:52:34 +0200

    Populate RecipeEdit form form with ingredients, enable add new inputs

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { RecipeService } from 'src/app/recipes/recipe.service';
 
 @Component({
@@ -26,26 +26,51 @@ export class RecipeEditComponent implements OnInit {
     this.initForm();
   }
 
+  get controls () {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
   private initForm () {
     let recipeName = '';
     let recipeImg = '';
     let description = '';
+    let ingredients = new FormArray([]);
     if (this.mode === 'edit') {
       const recipeToEdit = this._recipeService.getRecipeById(this.id);
       if (recipeToEdit) {
         recipeName = recipeToEdit.name;
         recipeImg = recipeToEdit.imagePath;
         description = recipeToEdit.description;
+        if (recipeToEdit.ingredients) {
+          for (let ing of recipeToEdit.ingredients) {
+            ingredients.push(
+              new FormGroup({
+                name: new FormControl(ing.name),
+                amount: new FormControl(ing.amount),
+              })
+            )
+          }
+        }
       }
     }
     this.recipeForm = new FormGroup({
       name: new FormControl(recipeName),
       imagePath: new FormControl(recipeImg),
-      description: new FormControl(description)
+      description: new FormControl(description),
+      ingredients
     })
   }
 
   onSubmit () {
     console.log(this.recipeForm);
+  }
+
+  addIngredient () {
+    this.controls.push(
+      new FormGroup({
+        name: new FormControl(),
+        amount: new FormControl()
+      })
+    )
   }
 }
